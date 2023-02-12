a44153e86bcbed194cb15bb1377094107d83eb63
Kirill <krikus.ms@gmail.com>
2023-02-12 14:37:11 +0100

    modified:   src/cells.wls 	modified:   src/core.js 	modified:   src/notebook.wls

--- src/core.js
+++ src/core.js
@@ -130,6 +130,15 @@ core.Rule = function (args, env) {
   return ["Rule", ...args];
 };
 
+core.Slot = function (args, env) {
+  return env.slot[interpretate(args[0], env)];
+}
+
+core.Function = function (args, env) {
+  env.todom = false;
+
+}
+
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
