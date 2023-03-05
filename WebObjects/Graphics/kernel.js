4a3a5f03c463e9828f3fafe66d10da89428f5d3a
JerryI <krikus.ms@gmail.com>
2023-03-05 15:29:43 +0100

    dynamic update

--- WebObjects/Graphics/kernel.js
+++ WebObjects/Graphics/kernel.js
@@ -4,10 +4,29 @@ var Plotly = require('plotly.js-dist');
 core.WListPlotly = function(args, env) {
     const arr = interpretate(args[0], env);
     console.log(arr);
+
     let newarr = [];
     arr.forEach(element => {
         newarr.push({x: element[0], y: element[1]}); 
     });
+
+    if (env.update === 'data') {
+      console.log('UPDATE DATA');
+
+      Plotly.animate(env.element, {
+        data: newarr,
+      }, {
+        transition: {
+          duration: 100,
+          easing: 'cubic-in-out'
+        },
+        frame: {
+          duration: 100
+        }
+      });     
+      return;
+    }
+
     Plotly.newPlot(env.element, newarr, {autosize: false, width: 500, height: 300, margin: {
         l: 30,
         r: 30,
