bbcce9d89c0b0556fb3856e08c9d22e88b9dc821
JerryI <krikus.ms@gmail.com>
2023-08-26 13:18:45 +0200

    darkmode

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    const o = createWindow(url);
+    const o = createWindow(globalURL);
     o.once('ready-to-show', () => {
       o.show()
     });
