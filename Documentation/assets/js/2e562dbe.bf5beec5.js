0e3edbb60c78d2c1fd5bc9810258831f0f087c2d
Kirill <krikus.ms@gmail.com>
2024-06-13 21:46:01 +0200

    docs update

---
+++ Documentation/assets/js/2e562dbe.bf5beec5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwlx_docs=self.webpackChunkwlx_docs||[]).push([[48],{20888:l=>{l.exports=JSON.parse('{"tag":{"label":"dev","permalink":"/blog/tags/dev","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/dev","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog title"}}')}}]);
---
+++ Documentation/assets/js/53a3e6dc.2f71e270.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwlx_docs=self.webpackChunkwlx_docs||[]).push([[7253],{26800:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":8,"blogDescription":"Blog","blogTitle":"Blog title"}}')}}]);
