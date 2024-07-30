b601ed54b08b0f14aab91cee8f8ae59a22d7c433
yloose <yanloose@gmail.com>
2024-07-30 22:32:32 +0200

    Add activation to container

--- container/run.sh
+++ container/run.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+LICENSE_DIR=/home/wljs/.WolframEngine/Licensing
 
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
@@ -9,16 +11,60 @@ usermod -o -u "$PUID" wljs
 chown -R wljs:wljs /wljs
 chown -R wljs:wljs /home/wljs
 
-LICENSE_DIR=/home/wljs/.WolframEngine/Licensing
 mkdir -p $LICENSE_DIR
+
+function activate_wolframscript {
+  
+  if [ -z ${WOLFRAMID_USERNAME+x} -o -z ${WOLFRAMID_PASSWORD+x} ]; then
+    # Manual activation
+    su - wljs -c "wolframscript -activate"
+    
+    if [ $? -neq 0 ]; then
+      echo "Activation failed, exiting.";
+      exit -1;
+    fi
+
+  else
+
+    su - wljs -c "expect << 'EOF'
+    spawn sh -c {wolframscript -activate}
+    
+    expect \"Wolfram ID:\" {send \"$WOLFRAMID_USERNAME\r\"}
+    expect \"Password:\" {send \"$WOLFRAMID_PASSWORD\r\"}
+    
+    lassign [wait] pid spawnpid os_error_flag value
+    
+    exit \$value
+    EOF"
+
+    if [ $? -neq 0 ]; then
+      echo "Activation with provided credentials failed.";
+      exit -1;
+    fi
+
+  fi
+
+  if [ ! -f $LICENSE_DIR/mathpass ]; then
+   echo "License file missing after activation."
+   exit -1;
+  else
+    # Activation success. Saving mathpass file
+    cp $LICENSE_DIR/mathpass /Licensing/mathpass
+  fi
+
+}
+
+# Check if license exists else continue
 if [ ! -f $LICENSE_DIR/mathpass ]; then
+  # Check if license is passed to container
   if [ -f /Licensing/mathpass ]; then
+    # Copy the license into the wolfram directory
     cp /Licensing/mathpass $LICENSE_DIR/mathpass
   else
-    echo "No license file to install."
+    activate_wolframscript
   fi
 fi
 chown -R wljs:wljs /home/wljs/.WolframEngine
 
-/usr/sbin/nginx
-su - wljs -c "/usr/bin/wolframscript -f /wljs/Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 docs 4003 folder '/workspace'"
+nginx
+su - wljs -c "wolframscript -f /wljs/Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 docs 4003 folder '/workspace'"
