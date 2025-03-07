2686f0e404b910b01aaa24ffe4b2ac6cd0af045b
Kirill Vasin <krikus.ms@gmail.com>
2025-03-07 23:35:27 +0300

    added activation via env

--- container/run.sh
+++ container/run.sh
@@ -2,15 +2,62 @@
 
 set -eux -o pipefail
 
+LICENSE_DIR=/root/.WolframEngine/Licensing
+
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
 groupmod -o -g "$PGID" wljs
 usermod -o -u "$PUID" wljs
 
+function activate_wolframscript {
+  
+  if [ -z ${WOLFRAMID_USERNAME+x} -o -z ${WOLFRAMID_PASSWORD+x} ]; then
+    # Manual activation
+    su - wljs -c "wolframscript -activate"
+    
+    if [ $? -ne 0 ]; then
+      echo "Activation failed, exiting.";
+      exit -1;
+    fi
+
+  else
+
+    su - wljs -c "expect << 'EOF'
+    spawn sh -c {wolframscript -activate}
+    
+    expect \"Wolfram ID:\" {send \"$WOLFRAMID_USERNAME\r\"}
+    expect \"Password:\" {send \"$WOLFRAMID_PASSWORD\r\"}
+    
+    lassign [wait] pid spawnpid os_error_flag value
+    
+    exit \$value
+    EOF"
+
+    if [ $? -ne 0 ]; then
+      echo "Activation with provided credentials failed.";
+      exit -1;
+    fi
+
+  fi
+
+  if [ -f $LICENSE_DIR/mathpass ]; then
+    # Activation success. 
+    echo "Success!"
+  else
+    echo "License file missing after activation."
+    exit -1;
+  fi
+
+}
+
+# Check if license exists else continue
+if [ ! -f $LICENSE_DIR/mathpass ]; then
+  activate_wolframscript
+fi
+
 chown -R wljs:wljs /wljs
 chown -R wljs:wljs /home/wljs
 
 nginx
-su - wljs -c "wolframscript -activate"
 su - wljs -c "wolframscript -f /wljs/Scripts/start.wls host 0.0.0.0 http 4000 ws 4001 ws2 4002 wsprefix ws ws2prefix ws2"
